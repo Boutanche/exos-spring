f79a28e8ad0f05eb6488bf97ca3a5fab4eb370ac
b.bertrand <benoit.bertrand@neo-soft.fr>
2021-10-13 17:08:36 +0200

    Initial commit

---
+++ src/main/java/org/github/boutanche/premier_exos/ExosSpringApplication.java
@@ -0,0 +1,14 @@
+package org.github.boutanche.premier_exos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExosSpringApplication.class, args);
+		System.out.println("Bonjour Benoit");
+	}
+
+}
---
+++ src/test/java/org/github/boutanche/premier_exos/ExosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.boutanche.premier_exos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
